021ecc9bddddec607728bdd04300f99d870c6358
ggluca12 <g.gluca12@gmail.com>
2021-03-14 18:47:13 +0100

    Image resources and compatibility

--- src/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ src/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,29 +3,39 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
+    lateinit var diceImage2 : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener { rollDice() }
-        val rollButtonCountUp: Button = findViewById(R.id.roll_button_CountUp)
-        rollButtonCountUp.setOnClickListener { rollDiceCountUp() }
+        rollButton.setOnClickListener {rollDice() }
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
+        /*val rollButtonCountUp: Button = findViewById(R.id.roll_button_CountUp)
+        rollButtonCountUp.setOnClickListener { rollDiceCountUp() }*/
     }
 
     private fun rollDice() {
-        /*Toast.makeText(this, "button clicked",
-                Toast.LENGTH_SHORT).show()*/
-        val resultText: TextView = findViewById(R.id.result_text)
-        //resultText.text = "Dice Rolled!"
-
-        val randomInt = (1..6).random()
-        resultText.text = randomInt.toString()
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
+    private fun getRandomDiceImage() : Int {
+        return when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
     }
 
+/*
     private fun rollDiceCountUp() {
         val resultText: TextView = findViewById(R.id.result_text)
 
@@ -39,6 +49,6 @@ class MainActivity : AppCompatActivity() {
             resultText.text=value.toString()
         }
 
-    }
+    }*/
 
 }
